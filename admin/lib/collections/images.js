57c7efade76dc43c556cd80f00f4f489a3ab3b29
John Crisostomo <jd@snapzio.com>
2016-05-27 17:21:21 +0800

    fixes allow / deny rules for images

--- admin/lib/collections/images.js
+++ admin/lib/collections/images.js
@@ -4,4 +4,18 @@ const Images = new FS.Collection('Images', {
   stores: [new FS.Store.GridFS('Images')],
 });
 
+Images.allow({
+  download: function() {
+    return true;
+  },
+  insert: function(userId) {
+    return !!userId;
+  },
+  update: function(userId) {
+    return !!userId;
+  },
+  remove: function(userId) {
+    return !!userId;
+  },
+});
 export default Images;
